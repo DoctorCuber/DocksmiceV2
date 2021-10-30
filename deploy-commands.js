e474511b4052e4008b9f5800d2f15c42526afdbe
DoctorCuber <72154273+DoctorCuber@users.noreply.github.com>
2021-10-30 13:47:36 -0700

    Continues to edit the Bot

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,3 +2,14 @@ const { SlashCommandBuilder } = require('@discordjs/bui;ders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientID, guildID, token} = require('./config.json');
+
+const commands = [
+//new SlashCommandBuilder().setname("Command Name").setDescription("Description of the command")
+
+].map(command => command.toJSON());
+
+const rest = new REST({ version:'1.2.13'}).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientID, guildID), {body: commands})
+    .then(() => console.log('Sucessfully loaded all commands.'))
+    .catch(console.error)
