ecdf9dd19d1dbe6f82726b36a24ed0c5e398209d
DoctorCuber <72154273+DoctorCuber@users.noreply.github.com>
2021-10-30 13:26:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Docksmice
+ Docksmice discord bot in discord.js.v13
---
+++ deploy-commands.js
@@ -0,0 +1,4 @@
+const { SlashCommandBuilder } = require('@discordjs/bui;ders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientID, guildID, token} = require('./config.json');
